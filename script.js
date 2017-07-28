ad1e971fc805b38710757b94f3bc41957a097551
Kevin Zhong <jeffrevin@gmail.com>
2017-07-28 15:56:34 +0000

    put functional pages

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // JavaScript File
 // JavaScript File
 $( document ).ready(function() {
-
-
+    $("#joke").click(function() {
+        alert("Knock! Knock! Who's there? Wanda. Wanda who? Wanda hang out with me right now?");
+    });
+    $("#submit").click(function() {
+        var guess = $("#enterField").val();
+        if(guess === "1999") {
+            alert("You got it!");
+        }
+        else if (guess < "1999"){
+            alert("Too low!");
+        }
+        else if (guess > "1999"){
+            alert("Too high!");
+        }
+    });
 });
